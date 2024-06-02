ce6deeaa522e78fbbaa0ca049004eba30d14a8c0
Runxi Yu <me@runxiyu.org>
2024-06-02 08:00:00 +0800

    Add C stuff

---
+++ c/oom.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	void *p;
+	char *c;
+	
+	for (int i=0;;++i) {
+		p = malloc(409600000);
+		printf("%d %p\n", i, p);
+		c = (char *) p;
+		c = 'a';
+	}
+
+	for (;;) {}
+}
---
+++ c/variable-names.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ 
+int main () {
+	int е = 1;
+	int e = 0;
+	printf("cyrillic: %d\n", е);
+	printf("latin: %d\n", e);
+	return 0;
+}
