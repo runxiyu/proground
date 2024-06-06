102e771a6fd1b300e257bc119148fcd6b3c273a5
Runxi Yu <me@runxiyu.org>
2024-06-06 08:00:00 +0800

    Meh

--- amd64/haxelf.s
+++ amd64/haxelf.s
@@ -6,39 +6,39 @@
 [section elf_header start=0]
 
 ELF_HEADER_START:
-db 0x7F, "E", "L", "F" ;        first 4 bytes
-db 0x01 ;                       64-bit
-db 0x01 ;                       little-endian
-db 0x01 ;                       version indicator, currently must be 1
-db 0x00 ;                       generic unix thing
-db 0x00 ;                       idk what this is
-times 16 - ($ - $$) db 0x00     ; padding
+db 0x7F, "E", "L", "F" ;      first 4 bytes
+db 0x01 ;                     64-bit
+db 0x01 ;                     little-endian
+db 0x01 ;                     version indicator, currently must be 1
+db 0x00 ;                     generic unix thing
+db 0x00 ;                     idk what this is
+times 16 - ($ - $$) db 0x00   ; padding
 
-dw 0x0002 ;                     executable thing
-dw 0x0003 ;                     x86_64
-dd 0x00000001 ;                 version again
-dd ram_start ;                  starting address
-dd PROGRAM_HEADER_START ;       where the program header is in the file
-dd 0x00000000 ;                 no section header
-dd 0x00000000 ;                 no flags
-dw ELF_HEADER_SIZE ;            needed
-dw PROGRAM_HEADER_SIZE ;        how large is each program header
-dw 0x0001 ;                     number of program headers
-dw 0x0000 ;                     no section size because no sections
-dw 0x0000 ;                     no sections
-dw 0x0000 ;                     no string header thing because no sections
+dw 0x0002 ;                   executable thing
+dw 0x0003 ;                   x86_64
+dd 0x00000001 ;               version again
+dd ram_start ;                starting address
+dd PROGRAM_HEADER_START ;     where the program header is in the file
+dd 0x00000000 ;               no section header
+dd 0x00000000 ;               no flags
+dw ELF_HEADER_SIZE ;          needed
+dw PROGRAM_HEADER_SIZE ;      how large is each program header
+dw 0x0001 ;                   number of program headers
+dw 0x0000 ;                   no section size because no sections
+dw 0x0000 ;                   no sections
+dw 0x0000 ;                   no string header thing because no sections
 
 ELF_HEADER_SIZE equ $ - ELF_HEADER_START
 
 PROGRAM_HEADER_START:
-dd 0x00000001 ;                 load this into memory
-dd file_start ;                 offset in file
-dd ram_start ;                  offset in virtual address
-dd 0x00000000 ;                 physical location, doesn't really matter
-dd EXECUTABLE_SECTION_SIZE ;    size in file
-dd EXECUTABLE_SECTION_SIZE ;    size in ram
-dd 0x00000007 ;                 read | execute | write, apparently possible
-dd 0x00000000 ;                 no alignment required
+dd 0x00000001 ;               load this into memory
+dd file_start ;               offset in file
+dd ram_start ;                offset in virtual address
+dd 0x00000000 ;               physical location, doesn't really matter
+dd EXECUTABLE_SECTION_SIZE ;  size in file
+dd EXECUTABLE_SECTION_SIZE ;  size in ram
+dd 0x00000007 ;               read | execute | write, apparently possible
+dd 0x00000000 ;               no alignment required
 
 PROGRAM_HEADER_SIZE equ $ - PROGRAM_HEADER_START
 
--- go/overflow.go
+++ go/overflow.go
@@ -7,6 +7,6 @@ import (
 func main () {
 	var i int = 9223372036854775807;
 	i = i + 1;
-	fmt.Println("%d", i);
+	fmt.Println(i);
 	// ah so Go doesn't specially handle overflows
 }
