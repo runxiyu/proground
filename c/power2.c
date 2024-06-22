3879771ff5ea1f733ddc3a2964dc324712cbd8d3
Runxi Yu <me@runxiyu.org>
2024-06-22 08:00:00 +0800

    Some status bar stuff

--- c/power2.c
+++ c/power2.c
@@ -4,27 +4,32 @@
 #include <sys/stat.h>
 #include <stdint.h>
 #include <err.h>
+#include <sys/mman.h>
 
+#define LEN 5
 
 int fd;
-char buf[4];
+char buf[LEN];
 struct stat sb;
+void *ptr;
 
-int main() {
-	buf[3] = '\n';
+int main()
+{
+	buf[LEN - 1] = '\n';
+	fd = open("/sys/class/power_supply/macsmc-battery/capacity", O_RDONLY);
+	read(fd, buf, LEN);
+	printf("%s", buf);
+	ptr = mmap(NULL, LEN, PROT_READ, MAP_SHARED, fd, 0);
+	if (ptr == MAP_FAILED) {
+		perror("mmap");
+		_exit(0);
+	}
 	for (;;) {
+		dprintf(1, "%p\n", ptr);
 		// There's gotta be a better way to do this than polling the battery
 		// file. I could use dbus to contact upower but I'm unfamiliar with
 		// dbus interrupts.
-		// Note that inotify won't work  with /sys/class files.
+		// Note that inotify won't work with /sys/class files.
 		// And I'm not in the mood of handling errors today.
-		fd = open("/sys/class/power_supply/macsmc-battery/capacity", O_RDONLY);
-		fstat(fd, &sb);
-		printf("%llu\n", (unsigned long long)(sb.st_size));
-		_exit(1);
-		read(fd, buf, sb.st_size);
-		lseek(fd, 0, SEEK_SET);
-		write(1, buf, sb.st_size);
-		close(fd);
 	}
 }
--- c/statusbar.c
+++ c/statusbar.c
@@ -1,21 +1,40 @@
+/*
+ * Stupid status bar.
+ * Set `status_command runxibar` in sway-bar.
+ * Written by Runxi Yu. Public domain.
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#define LEN 10
+
+time_t t;
+char buf[LEN];
+struct tm td;
+int fd;
+int i;
 
 int main()
 {
-	time_t t;
-	struct tm td;
+	fd = open("/sys/class/power_supply/macsmc-battery/capacity", O_RDONLY);
 	for (;;) {
+		pread(fd, buf, LEN, 0);
+		for (i = 0; i < LEN; ++i) {
+			if (buf[i] == '\n') {
+				buf[i] = '\0';
+				break;
+			}
+		}
 		t = time(NULL);
 		td = *localtime(&t);
-		printf("%d-%02d-%02d %02d:%02d:%02d\n",
+		dprintf(STDOUT_FILENO, "%s%% %d-%02d-%02d %02d:%02d:%02d\n",
+			buf,
 			td.tm_year + 1900,
 			td.tm_mon + 1,
-			td.tm_mday,
-			td.tm_hour,
-			td.tm_min,
-			td.tm_sec);
+			td.tm_mday, td.tm_hour, td.tm_min, td.tm_sec);
 		sleep(1);
 	}
 }
